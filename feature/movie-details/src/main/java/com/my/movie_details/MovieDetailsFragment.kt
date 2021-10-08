94bc372692ac85350c26af169fb70f0d0aefa225
Anton Mishanin <antonmishanin@mail.ru>
2021-10-08 20:52:31 +0700

    feat(movie-details): init module

--- feature/movie-details/src/main/java/com/my/movie_details/MovieDetailsFragment.kt
+++ feature/movie-details/src/main/java/com/my/movie_details/MovieDetailsFragment.kt
@@ -1,6 +1,5 @@
-package ru.androidschool.intensiv.ui.movie_details
+package com.my.movie_details
 
 import androidx.fragment.app.Fragment
-import ru.androidschool.intensiv.R
 
 class MovieDetailsFragment : Fragment(R.layout.movie_details_fragment)
