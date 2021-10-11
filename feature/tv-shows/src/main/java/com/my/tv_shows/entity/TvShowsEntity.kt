141f34ba4ee7ba7bbe1a5d20a45587ecc5472130
Anton Mishanin <antonmishanin@mail.ru>
2021-10-11 21:16:04 +0700

    ref(tv-shows): add annotation @DrawableRes

--- feature/tv-shows/src/main/java/com/my/tv_shows/entity/TvShowsEntity.kt
+++ feature/tv-shows/src/main/java/com/my/tv_shows/entity/TvShowsEntity.kt
@@ -1,7 +1,10 @@
 package com.my.tv_shows.entity
 
+import androidx.annotation.DrawableRes
+
 data class TvShowsEntity(
     val id: String,
+    @DrawableRes
     val imagePath: Int,
     val title: String,
     val rating: Float
