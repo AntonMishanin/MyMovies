cebc9134be138a80fa15155fb203619465d61496
Anton Mishanin <antonmishanin@mail.ru>
2021-10-18 20:05:17 +0700

    feat(data tv): replace network request with Rx

--- shared/data/tv/src/main/java/com/my/tv/remote/TvApi.kt
+++ shared/data/tv/src/main/java/com/my/tv/remote/TvApi.kt
@@ -1,10 +1,10 @@
 package com.my.tv.remote
 
 import com.my.tv.remote.dto.TvShowsResponse
-import retrofit2.Call
+import io.reactivex.Single
 import retrofit2.http.GET
 
 interface TvApi {
     @GET("tv/popular")
-    fun fetchPopularTvShows(): Call<TvShowsResponse>
+    fun fetchPopularTvShows(): Single<TvShowsResponse>
 }
