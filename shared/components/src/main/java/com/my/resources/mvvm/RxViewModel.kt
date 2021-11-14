5b304fdd724be1a224b3a47a577b318b73903d4d
Anton Mishanin <antonmishanin@mail.ru>
2021-11-14 15:11:46 +0700

    feat(shared mvvm): create base RxViewModel.kt

---
+++ shared/components/src/main/java/com/my/resources/mvvm/RxViewModel.kt
@@ -0,0 +1,17 @@
+package com.my.resources.mvvm
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+open class RxViewModel : ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+
+    protected fun Disposable.addToComposite() = compositeDisposable.add(this)
+}
