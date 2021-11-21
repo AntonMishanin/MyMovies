00e8fe23291f9514ebafc80eb0f732b43d23e9b6
Anton Mishanin <antonmishanin@mail.ru>
2021-11-21 21:33:50 +0700

    feat(feed): implement dagger

--- feature/feed/src/main/java/com/my/feed/di/FeedViewModelFactory.kt
+++ feature/feed/src/main/java/com/my/feed/di/FeedViewModelFactory.kt
@@ -4,8 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.my.domain.usecase.FetchCompositeMovieUseCase
 import com.my.feed.FeedViewModel
+import javax.inject.Inject
 
-class FeedViewModelFactory(
+class FeedViewModelFactory @Inject constructor(
     private val fetchCompositeMovieUseCase: FetchCompositeMovieUseCase
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
