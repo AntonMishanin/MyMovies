5477e01984e74df860efb10fddc979523e1691b3
Anton Mishanin <antonmishanin@mail.ru>
2022-06-06 17:34:24 +0700

    #20: add base-image-path to poster path

--- feature/tv-shows/src/main/java/com/my/tv_shows/data/converter/TvConverter.kt
+++ feature/tv-shows/src/main/java/com/my/tv_shows/data/converter/TvConverter.kt
@@ -1,12 +1,13 @@
 package com.my.tv_shows.data.converter
 
+import com.my.tv_shows.BuildConfig
 import com.my.tv_shows.data.remote.dto.TvShowDto
 import com.my.tv_shows.domain.TvShowsEntity
 
 internal object TvConverter {
     fun toValueObject(dto: TvShowDto): TvShowsEntity = TvShowsEntity(
         id = dto.id ?: throw NullPointerException("id must not be null"),
-        imagePath = dto.posterPath ?: "",
+        imagePath = BuildConfig.BASE_IMAGE_PATH + dto.posterPath,
         title = dto.name ?: "",
         rating = dto.voteAverage ?: 5f
     )
