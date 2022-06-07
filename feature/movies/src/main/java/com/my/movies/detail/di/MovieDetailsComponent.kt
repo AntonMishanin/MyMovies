b695537de1689610e2029df5779a9a01a3256aa9
Anton Mishanin <antonmishanin@mail.ru>
2022-06-07 11:32:06 +0700

    #11: add di to movies data

--- feature/movies/src/main/java/com/my/movies/detail/di/MovieDetailsComponent.kt
+++ feature/movies/src/main/java/com/my/movies/detail/di/MovieDetailsComponent.kt
@@ -2,13 +2,14 @@ package com.my.movies.detail.di
 
 import android.os.Bundle
 import com.my.core.di.Feature
+import com.my.movies.data.di.MoviesDataModule
 import com.my.movies.detail.presentation.MovieDetailsViewModel
 import dagger.BindsInstance
 import dagger.Component
 
 @Feature
 @Component(
-    modules = [MovieDetailsModule::class],
+    modules = [MovieDetailsModule::class, MoviesDataModule::class],
     dependencies = [MovieDetailsDependencies::class]
 )
 internal interface MovieDetailsComponent {
--- feature/movies/src/main/java/com/my/movies/detail/di/MovieDetailsModule.kt
+++ feature/movies/src/main/java/com/my/movies/detail/di/MovieDetailsModule.kt
@@ -1,12 +1,10 @@
 package com.my.movies.detail.di
 
-import android.content.Context
 import android.os.Bundle
 import com.my.core.di.Feature
 import com.my.favorite.domain.usecase.DeleteFromFavoriteByIdUseCase
 import com.my.favorite.domain.usecase.IsFavoriteByIdUseCase
 import com.my.favorite.domain.usecase.SaveMovieToFavoriteUseCase
-import com.my.movies.data.di.MoviesFactory
 import com.my.movies.detail.presentation.MovieDetailsViewModel
 import com.my.movies.domain.FetchMovieByIdUseCase
 import com.my.movies.domain.MovieRepository
@@ -35,10 +33,6 @@ internal class MovieDetailsModule {
     @Provides
     internal fun provideId(arguments: Bundle?) =
         arguments?.getString("id") ?: throw IllegalArgumentException("id must be not null")
-
-    @Provides
-    internal fun provideMoviesRepository(context: Context) =
-        MoviesFactory().provideMovieRepository(context)
 
     @Provides
     internal fun provideFetchMovieByIdUseCase(moviesRepository: MovieRepository) =
