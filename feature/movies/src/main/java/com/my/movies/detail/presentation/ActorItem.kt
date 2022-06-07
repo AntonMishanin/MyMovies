aa63e77b40c427e87d0fc20b13cbad5ddf3cc067
Anton Mishanin <antonmishanin@mail.ru>
2022-06-07 15:43:23 +0700

    #26: implement BaseItem.kt

--- feature/movies/src/main/java/com/my/movies/detail/presentation/ActorItem.kt
+++ feature/movies/src/main/java/com/my/movies/detail/presentation/ActorItem.kt
@@ -1,20 +1,15 @@
 package com.my.movies.detail.presentation
 
-import android.view.View
 import com.my.core.extensions.load
+import com.my.core.presentation.BaseItem
 import com.my.movies.R
 import com.my.movies.databinding.ItemActorBinding
 import com.my.movies.detail.entity.MovieEntity
-import com.xwray.groupie.viewbinding.BindableItem
 
 class ActorItem(
     private val content: MovieEntity.Actor,
     private val onItemClicked: (MovieEntity.Actor) -> Unit
-) : BindableItem<ItemActorBinding>() {
-
-    override fun getLayout(): Int = R.layout.item_actor
-
-    override fun initializeViewBinding(view: View) = ItemActorBinding.bind(view)
+) : BaseItem<ItemActorBinding>(R.layout.item_actor, ItemActorBinding::bind) {
 
     override fun bind(viewBinding: ItemActorBinding, position: Int) {
         viewBinding.actorName.text = content.name
--- feature/profile/src/main/java/com/my/profile/watchlist/MoviePreviewItem.kt
+++ feature/profile/src/main/java/com/my/profile/watchlist/MoviePreviewItem.kt
@@ -1,20 +1,15 @@
 package com.my.profile.watchlist
 
-import android.view.View
 import com.my.core.extensions.load
+import com.my.core.presentation.BaseItem
 import com.my.favorite.domain.usecase.FavoriteEntity
 import com.my.profile.R
 import com.my.profile.databinding.ItemSmallBinding
-import com.xwray.groupie.viewbinding.BindableItem
 
 class MoviePreviewItem(
     private val content: FavoriteEntity,
     private val onClick: (FavoriteEntity) -> Unit
-) : BindableItem<ItemSmallBinding>() {
-
-    override fun getLayout() = R.layout.item_small
-
-    override fun initializeViewBinding(view: View) = ItemSmallBinding.bind(view)
+) : BaseItem<ItemSmallBinding>(R.layout.item_small, ItemSmallBinding::bind) {
 
     override fun bind(viewBinding: ItemSmallBinding, position: Int) {
         viewBinding.imagePreview.setOnClickListener {
