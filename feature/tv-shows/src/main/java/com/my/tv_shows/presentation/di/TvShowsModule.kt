72cdd6e4e7082e91c74773e14d4b2e78e4cd88f7
Anton Mishanin <antonmishanin@mail.ru>
2022-06-08 12:43:33 +0700

    #22: replace tv-shows-singleton-converter with class

--- feature/tv-shows/src/main/java/com/my/tv_shows/presentation/di/TvShowsModule.kt
+++ feature/tv-shows/src/main/java/com/my/tv_shows/presentation/di/TvShowsModule.kt
@@ -1,6 +1,7 @@
 package com.my.tv_shows.presentation.di
 
 import com.my.tv_shows.data.TvRepositoryImpl
+import com.my.tv_shows.data.converter.TvShowsToDomainConverter
 import com.my.tv_shows.data.remote.TvApi
 import com.my.tv_shows.data.remote.TvRemoteDataSource
 import com.my.tv_shows.domain.FetchPopularTvShowsUseCase
@@ -22,12 +23,17 @@ internal class TvShowsModule {
         FetchPopularTvShowsUseCase(tvRepository)
 
     @Provides
-    fun provideTvRepository(remoteDataSource: TvRemoteDataSource): TvRepository =
-        TvRepositoryImpl(remoteDataSource)
+    fun provideTvRepository(
+        remoteDataSource: TvRemoteDataSource,
+        tvShowsToDomainConverter: TvShowsToDomainConverter
+    ): TvRepository = TvRepositoryImpl(remoteDataSource, tvShowsToDomainConverter)
 
     @Provides
     fun provideRemoteDataSource(tvApi: TvApi) = TvRemoteDataSource(tvApi)
 
     @Provides
     fun provideTvShowsApi(retrofit: Retrofit) = retrofit.create(TvApi::class.java)
+
+    @Provides
+    fun provideTvShowsToDomainConverter() = TvShowsToDomainConverter()
 }
