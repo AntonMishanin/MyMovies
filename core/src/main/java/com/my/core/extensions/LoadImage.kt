c95a54eeaf442e8d12c9b26f8901eb54885149c0
Anton Mishanin <antonmishanin@mail.ru>
2022-06-28 15:26:17 +0700

    #58: add placeholder

--- core/src/main/java/com/my/core/extensions/LoadImage.kt
+++ core/src/main/java/com/my/core/extensions/LoadImage.kt
@@ -1,16 +1,20 @@
 package com.my.core.extensions
 
 import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import com.my.core.R
 import com.squareup.picasso.Picasso
 
-fun ImageView.load(path: String) {
+fun ImageView.load(path: String, @DrawableRes placeholderResId: Int = R.drawable.ic_avatar) {
     Picasso.get()
         .load(path)
+        .placeholder(placeholderResId)
         .into(this)
 }
 
-fun ImageView.load(resId: Int) {
+fun ImageView.load(resId: Int, @DrawableRes placeholderResId: Int = R.drawable.ic_avatar) {
     Picasso.get()
         .load(resId)
+        .placeholder(placeholderResId)
         .into(this)
 }
