211300de927d3af102d440dbf52a52f4f4a3abff
Anton Mishanin <antonmishanin@mail.ru>
2022-06-28 11:30:41 +0700

    #53: di

--- feature/tv-shows/src/main/java/com/my/tv_shows/di/TvShowsModule.kt
+++ feature/tv-shows/src/main/java/com/my/tv_shows/di/TvShowsModule.kt
@@ -1,13 +1,6 @@
 package com.my.tv_shows.di
 
-import com.my.core.di.Feature
 import com.my.core.di.SchedulersWrapper
-import com.my.tv_shows.data.MemoryCacheDataSource
-import com.my.tv_shows.data.TvRepositoryImpl
-import com.my.tv_shows.data.converter.ToDomainExceptionConverter
-import com.my.tv_shows.data.converter.TvShowsToDomainConverter
-import com.my.tv_shows.data.remote.TvApi
-import com.my.tv_shows.data.remote.TvRemoteDataSource
 import com.my.tv_shows.domain.ObserveTvShowsUseCase
 import com.my.tv_shows.domain.RefreshTvShowsUseCase
 import com.my.tv_shows.domain.ToggleOverviewUseCase
@@ -16,7 +9,6 @@ import com.my.tv_shows.presentation.TvShowsPresenter
 import com.my.tv_shows.presentation.TvShowsUiConverter
 import dagger.Module
 import dagger.Provides
-import retrofit2.Retrofit
 
 @Module
 internal class TvShowsModule {
@@ -47,35 +39,6 @@ internal class TvShowsModule {
     @Provides
     fun provideClickOnTvShowsUseCase(tvRepository: TvRepository) =
         ToggleOverviewUseCase(tvRepository)
-
-    @Feature
-    @Provides
-    fun provideTvRepository(
-        remoteDataSource: TvRemoteDataSource,
-        tvShowsToDomainConverter: TvShowsToDomainConverter,
-        toDomainExceptionConverter: ToDomainExceptionConverter,
-        memoryCacheDataSource: MemoryCacheDataSource
-    ): TvRepository = TvRepositoryImpl(
-        remoteDataSource,
-        tvShowsToDomainConverter,
-        toDomainExceptionConverter,
-        memoryCacheDataSource
-    )
-
-    @Provides
-    fun provideMemoryCacheDataSource() = MemoryCacheDataSource()
-
-    @Provides
-    fun provideRemoteDataSource(tvApi: TvApi) = TvRemoteDataSource(tvApi)
-
-    @Provides
-    fun provideTvShowsApi(retrofit: Retrofit) = retrofit.create(TvApi::class.java)
-
-    @Provides
-    fun provideTvShowsToDomainConverter() = TvShowsToDomainConverter()
-
-    @Provides
-    fun provideToDomainExceptionConverter() = ToDomainExceptionConverter()
 
     @Provides
     fun provideTvShowsUiConverter() = TvShowsUiConverter()
