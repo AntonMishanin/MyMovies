d64a32cc8d9592a8125b8783155a1f7ed71858ed
Anton Mishanin <antonmishanin@mail.ru>
2022-06-30 14:55:58 +0700

    #31: add pagination to api service

--- feature/tv-shows/src/main/java/com/my/tv_shows/data/remote/TvApi.kt
+++ feature/tv-shows/src/main/java/com/my/tv_shows/data/remote/TvApi.kt
@@ -3,8 +3,14 @@ package com.my.tv_shows.data.remote
 import com.my.tv_shows.data.remote.dto.TvShowsResponse
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 internal interface TvApi {
-    @GET("tv/popular")
-    fun fetchPopularTvShows(): Single<TvShowsResponse>
+
+    @GET("tv/{type}")
+    fun fetchTvShows(
+        @Path("type") type: String,
+        @Query("page") page: Int
+    ): Single<TvShowsResponse>
 }
